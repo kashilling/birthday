c0e86c675af6d562558ea7b8396e5f77916efd4a
kashilling <kimashilling@gmail.com>
2014-11-19 20:52:59 -0500

    Initial Files

---
+++ app/models/concerns/owner.rb
@@ -0,0 +1,18 @@
+class Owner
+	def name
+		name = "Kim"
+	end
+    def birthdate
+	birthdate = Date.new(1973, 7, 16)
+	end
+	def countdown
+		today = Date.today
+		birthday = Date.new(today.year, birthdate.month, birthdate.day)
+		if birthday > today 
+			countdown = (birthday - today).to_i
+		elsif birthday == today 
+			return "Happy Birthday"
+		countdown = (birthday.next_year - today).to_i
+		end
+	end
+end
